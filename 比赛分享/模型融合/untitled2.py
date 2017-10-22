61de683937a0a4de76b1cee2e8cc2bec51329919
yang <2509039243@qq.com>
2017-10-22 15:11:56 +0800

    模型融合

---
+++ 比赛分享/模型融合/untitled2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 19 22:12:14 2017
+
+@author: Yang
+"""
+
+import pandas as pd
+
+train = pd.read_csv(r"G:\比赛分享\data\alltrain.csv")
+test= pd.read_csv(r"G:\比赛分享\data\alltest.csv")
